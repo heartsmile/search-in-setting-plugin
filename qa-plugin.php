56db637bf5f8b57852a4d165dd99a21a8daec30f
heartsmile <heart.smile.79@gmail.com>
2015-01-25 00:11:15 +0700

    update name

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,16 +1,16 @@
 <?php
 
 /*
-	Plugin Name: Search Setting
-	Plugin URI: https://github.com/heartsmile/search-setting-plugin
+	Plugin Name: Search In Setting
+	Plugin URI: https://github.com/heartsmile/search-in-setting-plugin
 	Plugin Description: Search a setting in administrator panel quickly
 	Plugin Version: 1.0
 	Plugin Date: 2014-11-10
-	Plugin Author: NHQT group
-	Plugin Author URI: http://www.facebook.com/heartsmile79
+	Plugin Author: NTHQ group
+	Plugin Author URI:
 	Plugin License: GPLv2
 	Plugin Minimum Question2Answer Version: 1.5
-	Plugin Update Check URI: https://github.com/heartsmile/search-setting-plugin
+	Plugin Update Check URI: https://github.com/heartsmile/search-in-setting-plugin
 */
 
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
@@ -27,12 +27,12 @@ qa_register_plugin_module(
 			'widget', 
 			'qa-search-setting-widget.php', 
 			'qa_search_setting_widget', 
-			'Search Setting Widget');
+			'Search In Setting Widget');
 qa_register_plugin_module(
 			'page', // type of module
 			'qa-search-setting-result.php', // PHP file containing module class
 			'qa_search_setting_result', // name of module class
-			'Search Setting Result' // human-readable name of module
+			'Search In Setting Result' // human-readable name of module
 );
 qa_register_plugin_phrases(
 	'qa-search-setting-lang-*.php', // pattern for language files
